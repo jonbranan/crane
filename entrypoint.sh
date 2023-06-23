b16875da2979abfa9d4fc626eaca837f7be00dcb
Jonathan Branan <jonbranan@gmail.com>
2023-06-23 03:06:59 -0500

    included supercronic

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
+
 CRON_CONFIG_FILE="/opt/crontab"
 
-# CRON
-get_env CRON
-CRON="${CRON:-"5 * * * *"}"
+echo "${CRON} python /opt/crane.py" > $CRON_CONFIG_FILE
 
-echo "${CRON} python /opt/crane.py" >> "${CRON_CONFIG_FILE}"
-
-exec supercronic -passthrough-logs -quiet "${CRON_CONFIG_FILE}"
+exec supercronic -passthrough-logs -quiet $CRON_CONFIG_FILE
