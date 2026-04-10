42bcf38fb77d2dce0ac61554f87e40db815733e5
jblu <jonbranan@gmail.com>
2026-04-10 16:54:04 -0500

    fixed crontab

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 CRON_CONFIG_FILE="/etc/crontabs/root"
 
-echo "${CRON} python /opt/crane.py" > $CRON_CONFIG_FILE
-echo "@reboot python /opt/crane.py" >> $CRON_CONFIG_FILE
+grep -qF '${CRON} python /opt/crane.py' $CRON_CONFIG_FILE ||echo "${CRON} python /opt/crane.py" >> $CRON_CONFIG_FILE
+grep -qF '@reboot' $CRON_CONFIG_FILE || echo "@reboot python /opt/crane.py" >> $CRON_CONFIG_FILE
 
 exec crond -f
